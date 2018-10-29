9fe243ba0fd4f52d9b95b1fa7e09b447dd3c2c4f
Will Douglas <willdowglas@gmail.com>
2018-10-29 19:56:05 -0300

    add ruby basics

---
+++ 02 - Ruby basics/001_name_age.rb
@@ -0,0 +1,7 @@
+print "Digite seu nome: "
+var1 = gets.chomp
+
+print "Digite sua idade: "
+var2 = gets.chomp
+
+p "Seu nome é #{var1}, e você tem #{var2} anos."
---
+++ 02 - Ruby basics/002_calc_2_numbers.rb
@@ -0,0 +1,10 @@
+print "Digite o primeiro número: "
+num1 = gets.chomp.to_i
+
+print "Digite o segundo número: "
+num2 = gets.chomp.to_i
+
+p "A soma dos números é: #{num1 + num2}"
+p "A subtração dos números é: #{num1 - num2}"
+p "A multiplicação dos números é: #{num1 * num2}"
+p "A divisão dos números é #{num1 / num2}"
